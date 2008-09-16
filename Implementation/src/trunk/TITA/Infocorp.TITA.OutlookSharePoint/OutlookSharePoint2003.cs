e2ba7c0a59475040841f5a0430e99150d54857b4
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-16 18:29:13 +0000

    Agrego nuevos Tipos en DTField

--- Implementation/src/trunk/TITA/Infocorp.TITA.OutlookSharePoint/OutlookSharePoint2003.cs
+++ Implementation/src/trunk/TITA/Infocorp.TITA.OutlookSharePoint/OutlookSharePoint2003.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.SharePoint;
 using System.IO;
+using Infocorp.TITA.DataTypes;
+using System.Collections.Specialized;
 
 namespace Infocorp.TITA.OutlookSharePoint
 {
@@ -12,52 +14,133 @@ namespace Infocorp.TITA.OutlookSharePoint
         {
         }
 
-        public static byte C2b(char c)
-        {
-            if ((int)c < 256) return (byte)c;
-            throw new Exception("character overflows a byte");
-        }
-
-        public static byte[] CA2b(char[] value)
-        {
-            byte[] buffer = new byte[value.Length];
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                buffer[i] = C2b(value[i]);
-            }
-            return buffer;
-        }
-
-
         #region IOutlookSharePoint Members
 
-        public bool AddIssue(string urlSite)
+        public bool AddIssue(string urlSite, DTIssue issue)
         {
+            try
+            {
+                using (SPSite site = new SPSite(urlSite))
+                {
+                    site.AllowUnsafeUpdates = true;
+                    using (SPWeb web = site.OpenWeb())
+                    {
+                        web.AllowUnsafeUpdates = true;
+                        SPList list = web.Lists["Issues"];
+                        SPListItem listItem = list.Items.Add();
+                        List<DTField> fieldCollection = issue.Fields;
+                        foreach (var field in fieldCollection)
+                        {
+                            listItem[field.Name] = field.Value;
+                        }
+                        SPAttachmentCollection listItemAttachmentCollection = listItem.Attachments;
+                        List<DTAttachment> attachmentCollection = issue.Attachments;
+                        foreach (var attachment in attachmentCollection)
+                        {
+                            listItemAttachmentCollection.Add(attachment.Name, attachment.Data);
+                        }
+                        listItem.Update();
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public List<DTField> GetFieldsIssue(string urlSite)
+        {
+            List<DTField> fieldsCollection = new List<DTField>();
             using (SPSite site = new SPSite(urlSite))
             {
-                site.AllowUnsafeUpdates = true;
                 using (SPWeb web = site.OpenWeb())
                 {
-                    web.AllowUnsafeUpdates = true;
                     SPList list = web.Lists["Issues"];
-                    SPListItem listItem = list.Items.Add();
-                    listItem["Title"] = "test2";
-                    listItem["Comment"] = "HOLA QUESO";
-                    SPAttachmentCollection attachmentCollection = listItem.Attachments;
-
-                    // for more help please see .net sdk
-                    FileStream fs = new FileStream("C:\\Test.txt", FileMode.Open, FileAccess.Read);
-                    char[] buffer = new char[1024];
-                    StreamReader sr = new StreamReader(fs);
-                    int retval = sr.ReadBlock(buffer, 0, 1024); // no. of bytes read
-                    Console.Write("Total Bytes Read = " + retval + "\n");
-                    sr.Close();
-
-                    attachmentCollection.Add("Test.txt", CA2b(buffer));
-                    listItem.Update();
+                    SPFieldCollection listFieldsCollection = list.Fields;
+                    foreach (SPField field in listFieldsCollection)
+	                {
+                        string name = field.Title;
+                        bool required = field.Required;
+                        SPFieldType type = field.Type;
+                        List<string> choices = new List<string>();
+                        
+                        if (type == SPFieldType.Choice)
+                        {
+                            StringCollection choicesCollection = ((SPFieldChoice)field).Choices;
+                            foreach (var choice in choicesCollection)
+                            {
+                                choices.Add(choice);
+                            }
+                        }
+                        switch (type)
+                        {
+                            case SPFieldType.Attachments:
+                                break;
+                            case SPFieldType.Boolean:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.Boolean, required, choices));
+                                break;
+                            case SPFieldType.Calculated:
+                                break;
+                            case SPFieldType.Choice:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.Choice, required, choices));
+                                break;
+                            case SPFieldType.Computed:
+                                break;
+                            case SPFieldType.Counter:
+                                break;
+                            case SPFieldType.CrossProjectLink:
+                                break;
+                            case SPFieldType.Currency:
+                                break;
+                            case SPFieldType.DateTime:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.DateTime, required, choices));
+                                break;
+                            case SPFieldType.Error:
+                                break;
+                            case SPFieldType.File:
+                                break;
+                            case SPFieldType.GridChoice:
+                                break;
+                            case SPFieldType.Guid:
+                                break;
+                            case SPFieldType.Integer:
+                                break;
+                            case SPFieldType.Invalid:
+                                break;
+                            case SPFieldType.Lookup:
+                                break;
+                            case SPFieldType.MaxItems:
+                                break;
+                            case SPFieldType.ModStat:
+                                break;
+                            case SPFieldType.MultiChoice:
+                                break;
+                            case SPFieldType.Note:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.Note, required, choices));
+                                break;
+                            case SPFieldType.Number:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.Integer, required, choices));
+                                break;
+                            case SPFieldType.Recurrence:
+                                break;
+                            case SPFieldType.Text:
+                                fieldsCollection.Add(new DTField(name, DTField.Types.String, required, choices));
+                                break;
+                            case SPFieldType.Threading:
+                                break;
+                            case SPFieldType.URL:
+                                break;
+                            case SPFieldType.User:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
                 }
             }
-            return true;
+            return fieldsCollection;
         }
 
         #endregion
