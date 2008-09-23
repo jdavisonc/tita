04a5a7dc2f7c1a15a2b662c90e5db6865388aff7
martin.almenar@gmail.com <martin.almenar@gmail.com@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-23 20:14:32 +0000

    Juntado el addin con toda la solucion

---
+++ trunk/Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/IHandlerAddIn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infocorp.TITA.WpfOutlookAddin
+{
+    /// <summary>
+    /// Interfaz para comunicarse con  IOutlookSharePoint
+    /// como manejador, y obtener datos para el AddIn
+    /// </summary>
+    public interface IHandlerAddIn
+    {
+        /// <summary>
+        /// Obtiene los contratos disponibles, para seleccionar uno para impacatar
+        /// </summary>
+        /// <returns>
+        /// Lista de DataUrls que contiene las urls disponibles para impactar incidentes
+        /// </returns>
+        List<DTUrl> GetUrlContracts();       
+
+        /// <summary>
+        /// Obtiene los campos que tiene que tener un incidente y 
+        /// crea una ventana con esos campos
+        /// </summary>
+        /// <params value="colSites">Coleccion de Sitios disponibles</params>
+        void BuildIncidentWindow();
+
+        /// <summary>
+        /// Obtiene los datos de la ventana y crea un Issue
+        /// para enviar a Sharepoint mediante la IOutlookSharePoint
+        /// </summary>
+        void BuildIssue();
+
+
+    }
+}
+
