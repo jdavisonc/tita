182464da5b8a7d32fa6ab98560900c522c36c97c
martin.almenar@gmail.com <martin.almenar@gmail.com@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-23 20:14:32 +0000

    Juntado el addin con toda la solucion

---
+++ Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/HandlerAddIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Infocorp.TITA.WpfOutlookAddin
+{
+    class HandlerAddIn:IHandlerAddIn
+    {
+        
+
+        #region IHandlerAddIn Members
+
+        public List<DTUrl> GetUrlContracts()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void BuildIncidentWindow()
+        {
+            
+            //IOutlookSharePoint
+            throw new NotImplementedException();
+        }
+
+        public void BuildIssue()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        
+    }
+}
