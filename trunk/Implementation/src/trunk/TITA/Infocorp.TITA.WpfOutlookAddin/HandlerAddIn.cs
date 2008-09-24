9af91b61449539f72843159b4f3ae8900e55f530
martin.almenar@gmail.com <martin.almenar@gmail.com@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-24 19:08:11 +0000

    Actualizados con el pattern Fabrica

--- trunk/Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/HandlerAddIn.cs
+++ trunk/Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/HandlerAddIn.cs
@@ -2,31 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using Infocorp.TITA.OutlookSharePoint;
+using Infocorp.TITA.WpfOutlookAddIn;
+using Infocorp.TITA.DataTypes;
 
 
 namespace Infocorp.TITA.WpfOutlookAddin
 {
     class HandlerAddIn:IHandlerAddIn
     {
+        private IOutlookSharePoint _outlookSP;
         
+        public HandlerAddIn()
+        {
+            _outlookSP = (new ControllerOutlookSharepoint()).GetOutlookSharepoint();
+
+        
+        }
 
         #region IHandlerAddIn Members
 
         public List<DTUrl> GetUrlContracts()
         {
-
-            throw new NotImplementedException();
+            List<DTUrl> oListDataUrl= new List<DTUrl>();
+            XmlDocument doc = new XmlDocument() ;
+            doc.Load("Contracts.xml");
+            XmlNodeList bookList = doc.GetElementsByTagName("Contract");
+            foreach (XmlNode node in bookList)
+            {
+                XmlElement bookElement = (XmlElement)node;
+                string title = string.Empty;
+                string url = string.Empty;
+                if (bookElement.HasAttributes)
+                {
+                    DTUrl oElementUrl = new DTUrl(bookElement.Attributes["Name"].InnerText, bookElement.Attributes["Url"].InnerText);
+                    oListDataUrl.Add(oElementUrl);
+                }
+            }
+            return oListDataUrl;
         }
 
-        public void BuildIncidentWindow()
+        public void BuildIncidentWindow(DTUrl url)
         {
             
-            //IOutlookSharePoint
-            throw new NotImplementedException();
+            List<DTField> oListaAtrib = _outlookSP.GetFieldsIssue(url.ContractUrl);
+            //aca generar la ventana para mostrar los campos del Issue
+
         }
+
 
         public void BuildIssue()
         {
+            //tomar los valores de la ventana y crear el issue para enviar
+            //a Sharepoint
+
             throw new NotImplementedException();
         }
 
