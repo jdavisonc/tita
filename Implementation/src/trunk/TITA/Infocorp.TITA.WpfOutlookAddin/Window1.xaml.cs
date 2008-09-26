2e1dc15114cfc7534aa81385325a6a72619272b0
martin.almenar <martin.almenar@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-26 00:37:05 +0000

    Enganchando las ventanas con el addin (version preliminar)

--- Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/Window1.xaml.cs
+++ Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/Window1.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Infocorp.TITA.WpfOutlookAddIn;
+using Infocorp.TITA.DataTypes;
 
 namespace Infocorp.TITA.WpfOutlookAddin
 {
@@ -19,9 +21,43 @@ namespace Infocorp.TITA.WpfOutlookAddin
     /// </summary>
     public partial class Window1 : Window
     {
-        public Window1()
+        //public Window1()
+        //{
+        //    InitializeComponent();
+        //}
+        //Label label1 = null;
+        //Button button1 = null;
+
+        public Window1(List<DTField> issueFields)
         {
             InitializeComponent();
+            WindowMaker oWindow = new WindowMaker(StackPanelDT, issueFields);
+            oWindow.GenerateWindow();
         }
+        /*
+              public void Generator(StackPanel mainPanel)
+              {
+            
+            
+
+          
+
+                  //TextBox text = new TextBox();
+                  //text.SetValue(Grid.ColumnProperty, 1);
+                  ComboBox text = new ComboBox();
+                  text.SetValue(Grid.ColumnProperty, 1);
+                  List<string> lista = new List<string>();
+                  lista.Add("hola");
+                  lista.Add("ddddddd");
+                  lista.Add("wwwwwww");
+                  text.ItemsSource = lista;
+
+                  grid.Children.Add(label1);
+                  grid.Children.Add(text);
+
+                  mainPanel.Children.Add(grid);
+           
+              }
+      */
     }
 }
