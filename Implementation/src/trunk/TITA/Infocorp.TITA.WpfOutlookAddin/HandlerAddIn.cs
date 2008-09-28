c885a870b2f922e5a59c6e3e8c8da865af0c5321
martin.almenar <martin.almenar@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-28 03:15:50 +0000

    Cambios para el merge de las ventanas

--- Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/HandlerAddIn.cs
+++ Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/HandlerAddIn.cs
@@ -7,16 +7,21 @@ using Infocorp.TITA.OutlookSharePoint;
 using Infocorp.TITA.WpfOutlookAddIn;
 using Infocorp.TITA.DataTypes;
 using System.Windows;
+using System.Windows.Controls;
+using AC.AvalonControlsLibrary.Controls;
 
 
 
 
 namespace Infocorp.TITA.WpfOutlookAddin
 {
-    public class HandlerAddIn:IHandlerAddIn
+    public class HandlerAddIn : IHandlerAddIn
     {
         private IOutlookSharePoint _outlookSP;
-        
+        private string _urlContract = string.Empty;
+        private Contracts _contract;
+        private Window1 _oIssueWindow;
+
         private static HandlerAddIn _instanceHandlerAddIn = null;
 
         private HandlerAddIn()
@@ -28,21 +33,69 @@ namespace Infocorp.TITA.WpfOutlookAddin
         {
             //lock (syncRoot)
             //{
-                if (_instanceHandlerAddIn == null)
-                {
-                    _instanceHandlerAddIn = new HandlerAddIn();
-                }
+            if (_instanceHandlerAddIn == null)
+            {
+                _instanceHandlerAddIn = new HandlerAddIn();
+            }
             //}
-            
+
             return _instanceHandlerAddIn;
         }
 
         #region IHandlerAddIn Members
 
-                public List<DTUrl> GetUrlContracts() 
+        private void btnContracts_Click(object sender, RoutedEventArgs e)
         {
-            List<DTUrl> oListDataUrl= new List<DTUrl>() ;
-            XmlDocument doc = new XmlDocument() ;
+
+            _urlContract = GetUrlContracts()[_contract.comboBox1.SelectedIndex].ContractUrl;
+            List<DTField> oListaAtrib = _outlookSP.GetFieldsIssue(_urlContract);
+            _contract.Close();
+
+            //aca genera la ventana para mostrar los campos del Issue
+            _oIssueWindow = new Window1(oListaAtrib);
+            _oIssueWindow.SendIssueButton.Click += new RoutedEventHandler(SendIssueButton_Click);
+            _oIssueWindow.Show();
+
+        }
+
+
+        private void SendIssueButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Dictionary<DTField,Control> dictionaryElements = _oIssueWindow.MapElements;
+            var keyElements = dictionaryElements.Keys;
+            foreach (DTField item in keyElements)
+            {
+                switch (item.Type)
+
+	            {
+		            case DTField.Types.Integer:
+                    case DTField.Types.String:
+                    case DTField.Types.Note:
+                        item.Value = ((TextBox)dictionaryElements[item]).Text;                        
+                        break;
+                    
+                    case DTField.Types.Choice:
+                    case DTField.Types.Boolean:
+                    case DTField.Types.User:
+                        item.Value = ((ComboBox)dictionaryElements[item]).SelectedValue.ToString();                        
+                        break;
+                    case DTField.Types.DateTime:
+                        item.Value = ((DatePicker)dictionaryElements[item]).CurrentlySelectedDate.ToString();                        
+                        break;
+                    default:
+                        break;
+	            }
+            }
+            List<DTField> oList = dictionaryElements.Keys;
+            BuildIssue(oList);
+
+        }
+
+        public List<DTUrl> GetUrlContracts()
+        {
+            List<DTUrl> oListDataUrl = new List<DTUrl>();
+            XmlDocument doc = new XmlDocument();
             string path = string.Empty;
             try
             {
@@ -51,11 +104,11 @@ namespace Infocorp.TITA.WpfOutlookAddin
             }
             catch (Exception e)
             {
-                MessageBox.Show("No se ha podido acceder al archivo Contracts.xml.\n\n"+
+                MessageBox.Show("No se ha podido acceder al archivo Contracts.xml.\n\n" +
                 "Verifique que el archivo se encuentre en " + path, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
                 throw e;
             }
-            
+
             XmlNodeList bookList = doc.GetElementsByTagName("Contract");
             foreach (XmlNode node in bookList)
             {
@@ -71,28 +124,29 @@ namespace Infocorp.TITA.WpfOutlookAddin
             return oListDataUrl;
         }
 
-        public void BuildIncidentWindow(DTUrl url)
+        public void BuildIncidentWindow()
         {
-            
-            List<DTField> oListaAtrib = _outlookSP.GetFieldsIssue(url.ContractUrl);
-            
-            //aca generar la ventana para mostrar los campos del Issue
-            Window1 oIssueWindow = new Window1(oListaAtrib);
-            oIssueWindow.Show();
+            _contract = new Contracts(GetUrlContracts());
+            _contract.btnContracts.Click += new RoutedEventHandler(btnContracts_Click);
+            _contract.Show();
+
+
+
         }
 
-        
 
-        public void BuildIssue(string urlSite, DTIssue issue)
+
+        public void BuildIssue(List<DTField> fields)
         {
             //tomar los valores de la ventana y crear el issue para enviar
             //a Sharepoint
-
-            _outlookSP.AddIssue(urlSite, issue);
+            List<DTAttachment> attachments= new List<DTAttachment>();
+            DTIssue oIssue = new DTIssue(fields, attachments);
+            _outlookSP.AddIssue(_urlContract, issue);
         }
 
         #endregion
 
-        
+
     }
 }
