ca811dc2dba63edbce1039f9944fd085644b79cf
martin.almenar <martin.almenar@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-28 00:26:11 +0000

    Ventana para seleccionar el contrato que corresponde al incidente

---
+++ Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/Contracts.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Infocorp.TITA.WpfOutlookAddin;
+
+namespace Infocorp.TITA.WpfOutlookAddIn
+{
+    /// <summary>
+    /// Interaction logic for Contracts.xaml
+    /// </summary>
+    public partial class Contracts : Window
+    {
+        private List<DTUrl> _contracts;
+
+        public Contracts(List<DTUrl> contracts)
+        {
+            InitializeComponent();
+            _contracts = contracts;
+            List<string> listContracts = new List<string>();
+            foreach (DTUrl item in contracts)
+            {
+                listContracts.Add(item.ContractName);
+            }
+            comboBox1.ItemsSource = listContracts;
+        }
+
+        private void btnContracts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
