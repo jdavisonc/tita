332833a87fdf56694045a61f765860d9acd6e478
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-09-30 02:00:11 +0000

    Creo SnapShot de la version 1.0 de TITA

---
+++ trunk/Implementation/src/tags/release-1.0/TITA/Infocorp.TITA.WITLogic/IWITServices.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infocorp.TITA.DataTypes;
+
+namespace Infocorp.TITA.WITLogic
+{
+    public interface IWITServices
+    {
+
+        void ApplyChanges(string siteUrl);
+
+        bool HasPendingChanges(string siteUrl);
+
+        #region WorkPackages
+
+        #endregion
+
+        #region Issues
+
+        DTIssue GetIssueTemplate(string urlSite);
+
+        List<DTIssue> GetIssues(string urlSite);
+
+        void AddNewIssue(DTIssue issue);
+
+        void ModifyIssue(DTIssue issue);
+
+        void DeleteIssue(int issueId);
+
+        #endregion
+
+        #region Tasks
+
+        #endregion
+
+        #region Contract
+
+        void AddNewContract(DTContract contract);
+
+        void DeleteContract(string contractId);
+
+        void ChangeCurrentContract(int contractId);
+
+        List<DTContract> GetContracts();
+
+        string GetContractSite(string contractId);
+
+        void ModifyContract(DTContract contract);
+
+        #endregion
+
+    }
+}
