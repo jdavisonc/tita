f5a8190f1c9b7ecee0687fd435e1ce736d6292ba
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-10-05 01:03:00 +0000

    Agrego nuevo Modelo de Datos DT.

---
+++ Implementation/src/trunk/TITA/Infocorp.TITA.DataTypes/DTFieldAtomicInteger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Infocorp.TITA.DataTypes
+{
+    [Serializable]
+    [DataContract]
+    public class DTFieldAtomicInteger : DTFieldAtomic
+    {
+        private int _value = 0;
+
+        public DTFieldAtomicInteger() : base()
+        {}
+        
+        public DTFieldAtomicInteger(string name, bool required, bool hidden, bool isReadOnly)
+            : base(name, required, hidden, isReadOnly)
+        {}
+
+        public DTFieldAtomicInteger(string name, bool required, bool hidden, bool isReadOnly, int value)
+            : base(name, required, hidden, isReadOnly)
+        {
+            _value = value;
+        }
+
+        public DTFieldAtomicInteger(DTFieldAtomicInteger dtFieldAtomicInteger)
+            : base((DTFieldAtomic)dtFieldAtomicInteger)
+        {
+            _value = dtFieldAtomicInteger.Value;
+        }
+
+        public override DTField.Types GetCustomType()
+        {
+            return Types.Integer;
+        }
+
+        [DataMember]
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+    }
+}
---
+++ Implementation/src/trunk/TITA/Infocorp.TITA.DataTypes/DTFieldChoiceLookup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Infocorp.TITA.DataTypes
+{
+    [Serializable]
+    [DataContract]
+    public class DTFieldChoiceLookup : DTFieldChoice
+    {
+        private string _lookupField;
+        private string _lookupList;
+
+        public DTFieldChoiceLookup() : base() {}
+
+        public DTFieldChoiceLookup(string name, bool required, bool hidden, bool isReadOnly, List<string> choices, string lookupField, string lookupList)
+            : base(name, required, hidden, isReadOnly, choices)
+        {
+            _lookupField = lookupField;
+            _lookupList = lookupList;
+        }
+
+        public DTFieldChoiceLookup(string name, bool required, bool hidden, bool isReadOnly, List<string> choices, string lookupField, string lookupList, string value)
+            : base(name, required, hidden, isReadOnly, choices, value)
+        {
+            _lookupField = lookupField;
+            _lookupList = lookupList;
+        }
+
+        public DTFieldChoiceLookup(DTFieldChoiceLookup dtFieldChoiceLookup)
+            : base((DTFieldChoice)dtFieldChoiceLookup)
+        {
+            _lookupField = dtFieldChoiceLookup.LookupField;
+            _lookupList = dtFieldChoiceLookup.LookupList;
+        }
+
+        public override DTField.Types GetCustomType()
+        {
+            return Types.Lookup;
+        }
+
+        [DataMember]
+        public string LookupField
+        {
+            get { return _lookupField; }
+        }
+
+        [DataMember]
+        public string LookupList
+        {
+            get { return _lookupList; }
+        }
+    }
+}
