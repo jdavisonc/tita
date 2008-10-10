c34ca94325ccf79d5ba18a238e04dfee00324e74
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-10-10 22:23:52 +0000

    Agrego Trim() a cada asignacion.

--- Implementation/src/trunk/TITA/Infocorp.TITA.DataTypes/DTContract.cs
+++ Implementation/src/trunk/TITA/Infocorp.TITA.DataTypes/DTContract.cs
@@ -14,42 +14,42 @@ namespace Infocorp.TITA.DataTypes
         public string ContractId
         {
             get { return _contractId; }
-            set { _contractId = value; }
+            set { _contractId = value.Trim(); }
         }
         private string _site;
         [DataMember]
         public string Site
         {
             get { return _site; }
-            set { _site = value; }
+            set { _site = value.Trim(); }
         }
         private string _userName;
         [DataMember]
         public string UserName
         {
             get { return _userName; }
-            set { _userName = value; }
+            set { _userName = value.Trim(); }
         }
         private string _issuesList;
         [DataMember]
         public string issuesList
         {
             get { return _issuesList; }
-            set { _issuesList = value; }
+            set { _issuesList = value.Trim(); }
         }
         private string _workPackageList;
         [DataMember]
         public string workPackageList
         {
             get { return _workPackageList; }
-            set { _workPackageList = value; }
+            set { _workPackageList = value.Trim(); }
         }
         private string _taskList;
         [DataMember]
         public string taskList
         {
             get { return _taskList; }
-            set { _taskList = value; }
+            set { _taskList = value.Trim(); }
         }
 
     }
