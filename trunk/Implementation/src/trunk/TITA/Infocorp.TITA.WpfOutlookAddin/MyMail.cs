ca33350869ac552d894d6100d35e9b704a91235f
martin.almenar <martin.almenar@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-10-11 18:38:06 +0000

    Cambios para que realice los attachs (Probado , genera el attach al incidente con el mails y los attachs que tenga este)

--- trunk/Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/MyMail.cs
+++ trunk/Implementation/src/trunk/TITA/Infocorp.TITA.WpfOutlookAddin/MyMail.cs
@@ -7,23 +7,35 @@ using System.Runtime.Serialization;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Microsoft.Office.Interop.Outlook;
+using System.Runtime.Serialization;
 
 namespace Infocorp.TITA.WpfOutlookAddin
 {
 
     [Serializable]
+    [DataContract]
     public class MyMail : ISerializable
     {
-        MailItem _myMail;
+
+        MailItem _myMailObject;
+
+        [DataMember]
+        public MailItem MyMailSerilize
+        {
+            get { return _myMailObject; }
+            set { _myMailObject = value; }
+        }
+
+        public MyMail() { }
 
         public MyMail(MailItem mail)
         {
-            _myMail = mail;
+            _myMailObject = mail;
         }
 
         public MailItem GetMail()
         {
-            return _myMail;
+            return _myMailObject;
         }
 
         void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
@@ -38,9 +50,18 @@ namespace Infocorp.TITA.WpfOutlookAddin
         {
             MemoryStream memoryStream = new MemoryStream();
             BinaryFormatter binaryFormatter = new BinaryFormatter();
-            MyMail myMailObject = new MyMail(_myMail);
-            binaryFormatter.Serialize(memoryStream, myMailObject);
-            return memoryStream.ToArray();
+            //MyMail myMailObject = new MyMail(_myMail)
+            string fileTmpName = System.IO.Path.GetTempFileName();
+            _myMailObject.SaveAs(fileTmpName,OlSaveAsType.olMSG);
+            FileStream oFile = new FileStream(fileTmpName,FileMode.Open, FileAccess.Read);
+            byte[] imageData = new byte[oFile.Length];
+            oFile.Read(imageData,0,(int)oFile.Length);
+            oFile.Close();
+            //binaryFormatter.Serialize(memoryStream
+            
+            //return memoryStream.ToArray();
+
+            return imageData;
         }
 
         /*
