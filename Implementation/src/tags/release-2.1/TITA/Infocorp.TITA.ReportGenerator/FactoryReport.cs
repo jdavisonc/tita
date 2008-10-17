1476cd83ae1227012b1b790995af226dbca583da
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-10-17 01:56:31 +0000

    Version 2.1 - Con pequeños cambios en la UI

---
+++ Implementation/src/tags/release-2.1/TITA/Infocorp.TITA.ReportGenerator/FactoryReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infocorp.TITA.ReportGenerator
+{
+    public class FactoryReport
+    {
+        private static IReportGenerator _instance = null;
+        public static IReportGenerator GetInstance()
+        {
+            if (_instance == null)
+                return _instance = new ControladorRG();
+            return _instance;
+        }
+    }
+}
---
+++ Implementation/src/tags/release-2.1/TITA/Infocorp.TITA.WITLogic.Tests/WITServices_WP_Test.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Infocorp.TITA.SharePointUtilities;
+using Infocorp.TITA.DataTypes;
+
+namespace Infocorp.TITA.WITLogic.Tests
+{
+[TestFixture]
+    public class WITServices_WP_Test
+    {
+        [Test]
+        public void AddWP()
+        {
+            string siteUrl = "1";
+            ISharePoint sharepoint = SharePointUtilities.SharePointUtilities.GetInstance().GetISharePoint();            
+            DTItem issue = new DTItem();
+            issue.Attachments = new List<DTAttachment>();
+            issue.Fields = sharepoint.GetFieldsWorkPackage(siteUrl);
+            string name = string.Empty;
+            foreach (DTField field in issue.Fields)
+            {
+                try
+                {
+                    if (!field.Hidden && !field.IsReadOnly)
+                    {
+                        switch (field.GetCustomType())
+                        {
+                            case DTField.Types.Number:
+                                (field as DTFieldAtomicNumber).Value = 0;
+                                break;
+                            case DTField.Types.String:
+                                (field as DTFieldAtomicString).Value = "0";
+                                break;
+                            case DTField.Types.Choice:
+                                (field as DTFieldChoice).Value = (field as DTFieldChoice).Choices.First();
+                                break;
+                            case DTField.Types.Boolean:
+                                (field as DTFieldAtomicBoolean).Value = false;
+                                break;
+                            case DTField.Types.DateTime:
+                                (field as DTFieldAtomicDateTime).Value = DateTime.Today.AddDays(5);
+                                break;
+                            case DTField.Types.Note:
+                                (field as DTFieldAtomicNote).Value = "Nota";
+                                break;
+                            case DTField.Types.User:
+                                (field as DTFieldChoiceUser).Value = (field as DTFieldChoiceUser).Choices.First();
+                                break;
+                            case DTField.Types.Counter:
+
+                                break;
+                            case DTField.Types.Lookup:
+                                break;
+                            case DTField.Types.Default:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    string msg = exc.Message;
+                }
+            }
+            try
+            {
+                int originalCount = sharepoint.GetWorkPackages(siteUrl, string.Empty).Count;
+                WITFactory.Instance().WITServicesInstance().AddWorkPackage(issue, siteUrl);
+                WITFactory.Instance().WITServicesInstance().ApplyChanges(siteUrl);
+
+                int newCount = sharepoint.GetWorkPackages(siteUrl, string.Empty).Count;
+
+                Assert.AreEqual(originalCount + 1, newCount);
+            }
+            catch (Exception exc)
+            {
+                Assert.Fail("No se agregó el wp. ", exc.Message);
+
+            }
+
+        }
+    }
+}
