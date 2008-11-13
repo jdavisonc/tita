d33f765b2d0c8ff6f3fda5814d5fca77f03ce5ae
jorge.sofpc <jorge.sofpc@cf6e0173-bc54-0410-ad18-b5d2e1e173ec>
2008-11-13 18:19:47 +0000

    Version 4.0

---
+++ trunk/Implementation/src/tags/release-4.0/TITA/Infocorp.TITA.DataTypes/DTFieldChoice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Infocorp.TITA.DataTypes
+{
+    [Serializable]
+    [DataContract]
+    public class DTFieldChoice : DTField
+    {
+        private List<string> _choices = null;
+        private string _value = string.Empty;
+
+        public DTFieldChoice() : base() {}
+
+        public DTFieldChoice(string name, string internalName, bool required, bool hidden, bool isReadOnly, List<string> choices)
+            : base(name, internalName, required, hidden, isReadOnly)
+        {
+            _choices = new List<string>(choices);
+        }
+
+        public DTFieldChoice(string name, string internalName, bool required, bool hidden, bool isReadOnly, List<string> choices, string value)
+            : base(name, internalName, required, hidden, isReadOnly)
+        {
+            _choices = new List<string>(choices);
+            _value = value;
+        }
+
+        public DTFieldChoice(DTFieldChoice dtFieldChoice)
+            : base((DTField)dtFieldChoice)
+        {
+            _value = dtFieldChoice.Value;
+            _choices = new List<string>(dtFieldChoice.Choices);
+        }
+
+        public override DTField.Types GetCustomType()
+        {
+            return Types.Choice;
+        }
+
+        [DataMember]
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        [DataMember]
+        public List<string> Choices
+        {
+            get { return _choices; }
+            set { _choices = value; }
+        }
+    }
+}
---
+++ trunk/Implementation/src/tags/release-4.0/TITA/Infocorp.TITA.ReportGenerator/FactoryReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infocorp.TITA.ReportGenerator
+{
+    public class FactoryReport
+    {
+        private static  FactoryReport _instance = null;
+        private static IReportGenerator _instanceIReportGenerator = null;
+        public static FactoryReport GetInstance()
+        {
+            if (_instance == null)
+                return _instance = new FactoryReport();
+            return _instance;
+        }
+        public IReportGenerator GetIReportGenerator()
+        {
+            if (_instanceIReportGenerator == null)
+                _instanceIReportGenerator = new ControladorRG();
+            return _instanceIReportGenerator;
+        }
+    }
+}
